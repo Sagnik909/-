89308747f7f7e719b6d2c4db0423a8d3acdbfee3
Sagnik909 <90745613+Sagnik909@users.noreply.github.com>
2024-03-27 18:05:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ function preload() {
 }
 
 function setup() {
-  canvas = createCanvas(300, 300);
+  canvas = createCanvas(300, 500);
   canvas.center();
   video = createCapture(VIDEO);
-  video.size(300, 500);
+  video.size(300, 300);
   video.hide();
 
   poseNet = ml5.poseNet(video, modelLoaded);
